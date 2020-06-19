fd4ecd2cfc92489ade7f02d9a5ac2c996b094d08
lycharlie <lyscharlie@hotmail.com>
2020-06-19 11:17:15 +0800

    111

--- src/main/java/com/lyscharlie/biz/mapper/AreaMapper.java
+++ src/main/java/com/lyscharlie/biz/mapper/AreaMapper.java
@@ -1,8 +1,11 @@
 package com.lyscharlie.biz.mapper;
 
+import org.springframework.stereotype.Repository;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lyscharlie.biz.entity.AreaDO;
 
+@Repository
 public interface  AreaMapper extends BaseMapper<AreaDO> {
 
 }
--- src/main/java/com/lyscharlie/biz/mapper/UserMapper.java
+++ src/main/java/com/lyscharlie/biz/mapper/UserMapper.java
@@ -1,8 +1,11 @@
 package com.lyscharlie.biz.mapper;
 
+import org.springframework.stereotype.Repository;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lyscharlie.biz.entity.UserDO;
 
+@Repository
 public interface UserMapper extends BaseMapper<UserDO> {
 
 	public Integer selectCountByRegisterReferId(Long registerReferId);
