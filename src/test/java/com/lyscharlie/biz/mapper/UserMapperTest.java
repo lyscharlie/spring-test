8a29bfc97f59ebde26d58335955ee135e16c519e
lyscharlie <lyscharlie@hotmail.com>
2020-08-06 10:36:50 +0800

    111

--- src/test/java/com/lyscharlie/biz/mapper/UserMapperTest.java
+++ src/test/java/com/lyscharlie/biz/mapper/UserMapperTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.lyscharlie.SpringTestApplication;
+import com.lyscharlie.biz.entity.UserDO;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringTestApplication.class)
@@ -22,5 +23,19 @@ public class UserMapperTest {
 		Integer count = this.userMapper.selectCountByRegisterReferId(registerReferId);
 		System.out.println(count);
 		Assert.assertTrue(count > 0);
+	}
+
+	@Test
+	public void updateById() {
+		// 指定字段修改
+		// UserDO user = new UserDO();
+		// user.setUserId(1L);
+		// user.setEmail("aaa@aaa.com");
+		// Assert.assertTrue(this.userMapper.updateById(user) > 0);
+
+		// 全部字段修改
+		UserDO user = this.userMapper.selectById(1L);
+		user.setEmail("lyscharlie@abc.com");
+		Assert.assertTrue(this.userMapper.updateById(user) > 0);
 	}
 }
